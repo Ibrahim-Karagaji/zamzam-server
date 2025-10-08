230bddd6a98ad19061774f4a9b0b3d7006a98af3
Abdullah T. <Alsktawaybwd@gmail.com>
2025-10-08 14:07:59 +0300

    I teased all the controllers all returning 200  successfuly

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReportController;
+use App\Http\Controllers\HotelController;
+use App\Http\Controllers\DistributionController;
 use Illuminate\Support\Facades\Route;
 
-Route::delete("/test", [UserController::class, "registry"]);
-Route::post("/post", [UserController::class, "post"]);
-Route::put("/update", [UserController::class, "update"]);
-Route::put("/update", [UserController::class, "update"]);
-Route::get("/get", [UserController::class, "get"]);
+Route::get("/user", [UserController::class, "registry"]);
+Route::get("/report", [ReportController::class, "report"]);
+Route::get("/hotel", [HotelController::class, "hotel"]);
+Route::get("/Distrbution", [DistributionController::class, "Distrbution"]);
+
+
