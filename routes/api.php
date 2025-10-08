596896d57248709b49bf13f87984c395f73b7af0
Abdullah T. <Alsktawaybwd@gmail.com>
2025-10-08 03:37:30 +0300

    this is a test

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::delete("/test", [UserController::class, "registry"]);
 Route::post("/post", [UserController::class, "post"]);
 Route::put("/update", [UserController::class, "update"]);
+Route::put("/update", [UserController::class, "update"]);
 Route::get("/get", [UserController::class, "get"]);
